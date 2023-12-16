5371160d440c83af05b93e830b3b7723f92f4b56
ashot-k <32601799+ashot-k@users.noreply.github.com>
2023-12-16 13:44:04 +0200

    Update Client.java

--- src/main/java/com/hotel/entity/client/Client.java
+++ src/main/java/com/hotel/entity/client/Client.java
@@ -6,37 +6,11 @@ import jakarta.validation.constraints.Pattern;
 
 @Entity
 public class Client {
-    public static final String PASSWORD_ERROR_MESSAGE = """
-            Please enter a password that has: <br>
-            At least 5 characters. <br>
-            At least one letter. <br>
-            At least one number.""";
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
+    
     private Long id;
     private String username;
-    @Column(name = "pass", nullable = false, length = 1000)
-    @NotBlank(message = "Enter valid password")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d).{5,}$"
-            , message = PASSWORD_ERROR_MESSAGE)
+
     private String password;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     public Long getId() {
