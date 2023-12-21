3be63c70e214542f494375dca8fac359605434cc
ashot <karamanidist@gmail.com>
2023-12-21 18:46:54 +0200

    created Room entity , RoomService and RoomRestController

--- src/main/java/com/hotel/controller/RoomRestController.java
+++ src/main/java/com/hotel/controller/RoomRestController.java
@@ -1,13 +1,11 @@
 package com.hotel.controller;
 
 import com.hotel.entity.room.Room;
+import com.hotel.entity.user.Person;
 import com.hotel.service.RoomService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,9 +18,28 @@ public class RoomRestController {
         this.roomService = roomService;
     }
 
+    @GetMapping("/{roomId}")
+    public ResponseEntity<Room> getRooms(@PathVariable Long roomId) {
+        return new ResponseEntity<>(roomService.getRoomById(roomId), HttpStatus.OK);
+    }
+
     @GetMapping("/all")
     @ResponseBody
     public ResponseEntity<List<Room>> getRooms(){
         return new ResponseEntity<>(roomService.findAllRooms(), HttpStatus.OK);
+    }
+    @PostMapping("/create-room")
+    public ResponseEntity<Room> createRoom(@RequestBody Room room) {
+        return new ResponseEntity<>(roomService.saveRoom(room), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update-room/{id}")
+    public ResponseEntity<Room> updateRoom(@RequestBody Room updatedRoom, @PathVariable Long id) {
+        return new ResponseEntity<>(roomService.updateRoom(id, updatedRoom), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete-user/{roomId}")
+    public ResponseEntity<String> deleteRoom(@PathVariable String roomId) {
+        return new ResponseEntity<>(roomService.deleteRoom(Long.parseLong(roomId)), HttpStatus.OK);
     }
 }
